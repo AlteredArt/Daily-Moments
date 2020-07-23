fb3aaeb90e92a12a10975c9557105cffa9b88858
Altered Art <cherokeedreemer@gmail.com>
2020-07-23 17:45:39 -0600

    move to firebase

---
+++ src/data.ts
@@ -0,0 +1,17 @@
+export const entries = [
+  {
+    id: '1',
+    title: 'First Entry',
+    description: 'My first entry',
+  },
+  {
+    id: '2',
+    title: 'second Entry',
+    description: 'My second entry',
+  },
+  {
+    id: '3',
+    title: 'Third Entry',
+    description: 'My third entry',
+  },
+];
